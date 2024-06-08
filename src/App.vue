377d1a617fde040ee0aff4b7b042dd2a34a5321f
YJS96 <wnt9864@hotmail.com>
2024-06-08 22:34:43 +0900

    init: 프로젝트 생성

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
+
+<style>
+/* Pretendard */
+@import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css');
+
+* {
+  font-family: 'Pretendard Variable', Pretendard, -apple-system,
+    BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
+    'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
+    'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+}
+</style>
