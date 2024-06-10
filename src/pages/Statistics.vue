32f78aed506a720dc9afde0d6e67b85b791e0edc
Choi ho jin <jkhh9725@gmail.com>
2024-06-11 00:49:09 +0900

    feat: Statistics 페이지 컴포넌트 생성

---
+++ src/pages/Statistics.vue
@@ -0,0 +1,23 @@
+<template>
+  <Main :padded="true" class="CalenderWarpper">
+    <Calender />
+  </Main>
+</template>
+
+<script setup>
+import Calender from "@/components/Calender.vue";
+</script>
+
+<style>
+.CalenderWarpper {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.vc-container {
+  width: 80% !important;
+}
+</style>
