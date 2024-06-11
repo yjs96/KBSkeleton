0ba8212efa1bef7486c9bce0060f8ed7ad87a935
Choi ho jin <jkhh9725@gmail.com>
2024-06-11 19:59:55 +0900

    feat: mypage 컴포넌트 생성

---
+++ src/pages/Mypage.vue
@@ -0,0 +1,121 @@
+<template>
+  <Main :padded="true" class="mypage-wrapper">
+    <!-- user header -->
+    <div class="mypage-container">
+      <div class="upper-bar">
+        <div class="user-name-frame">
+          <span>박소해</span>
+          <span class="weight-regular">님</span>
+          <i class="fa solid fa-chevron-right"></i>
+        </div>
+        <div class="profile-frame">프사</div>
+      </div>
+      <div class="user-box">
+        <div class="a"></div>
+        <div class="b">b</div>
+        <div class="c">c</div>
+        <div class="d">
+          <div class="d-style">
+            <div class="d1">d1</div>
+            <div class="d1">d2</div>
+            <div class="d1">d3</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Main>
+</template>
+
+<script setup>
+import Main from '@/components/Main.vue';
+</script>
+
+<style scoped>
+.mypage-wrapper {
+  display: flex;
+  flex-direction: column;
+  background-color: var(--gray);
+}
+.mypage-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.upper-bar {
+  margin-top: 14px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* border: 1px solid black; */
+  border-radius: 10px;
+}
+
+.user-name-frame {
+  font-size: 22px;
+  display: flex;
+  align-items: center;
+}
+i {
+  font-size: 16px;
+  margin: 0 2px 0 8px;
+}
+.profile-frame {
+  width: 40px;
+  height: 40px;
+  background-color: var(--gray);
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  object-fit: cover;
+}
+/* card */
+.user-box {
+  display: flex;
+  flex-direction: column;
+  height: 80vh;
+  background-color: var(--green);
+  border-radius: 10px;
+
+  box-shadow: 5px 5px 7px 0px rgba(182, 182, 182, 0.645);
+}
+.a {
+  flex: 1;
+}
+.b {
+  flex: 1;
+}
+.c {
+  flex: 1;
+}
+.d {
+  height: 400px;
+  /* border: 1px solid red; */
+  background-color: var(--white);
+  border-radius: 0px 0px 10px 10px;
+}
+.d-style {
+  padding: 40px 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+
+  /* border: 1px solid red; */
+}
+.d1 {
+  width: 400px;
+  height: 80px;
+  /* border: 1px solid red; */
+
+  border-radius: 10px;
+  box-shadow: 5px 5px 7px 0px rgba(182, 182, 182, 0.645);
+  transition: all 0.2s ease-in;
+}
+.d1:hover {
+  transform: translate(10px, -10px);
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Home from '@/pages/Home.vue';
 import Statistics from '@/pages/Statistics.vue';
 import Charts from '@/pages/Charts.vue';
+import Mypage from '@/pages/Mypage.vue';
 import { createRouter, createWebHistory } from 'vue-router';
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +21,12 @@ const router = createRouter({
       path: '/three',
       name: 'statistics',
       component: Statistics,
-    }
+    },
+    {
+      path: '/four',
+      name: 'mypage',
+      component: Mypage,
+    },
   ],
 });
 
