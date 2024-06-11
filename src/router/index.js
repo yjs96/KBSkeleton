2fe3de6dee4939d5f47d264f0e881d51c0bdc658
Choi ho jin <jkhh9725@gmail.com>
2024-06-11 17:20:51 +0900

    feat: Statistics 경로 three로 변경

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
-import Home from "@/pages/Home.vue";
-import Statistics from "@/pages/Statistics.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import Home from '@/pages/Home.vue';
+import Statistics from '@/pages/Statistics.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: Home,
     },
     {
-      path: "/two",
-      name: "statistics",
+      path: '/three',
+      name: 'statistics',
       component: Statistics,
     },
   ],
